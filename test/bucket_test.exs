d82148f0c60e76b32f66a27adc8e621c5e314d57
Alexandra Collins <andracolexa@gmail.com>
2022-01-04 04:43:57 +0100

    implement Agent a simple wrapper around state
    - implement key-value bucket module
    - implement funtions for starting, getting and updating the agent state
    - write test for key-value bucket module

---
+++ test/bucket_test.exs
@@ -0,0 +1,16 @@
+defmodule KV.BucketTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    {:ok, bucket} = KV.Bucket.start_link([])
+    %{bucket: bucket}
+  end
+
+  test "store values by key", %{bucket: bucket} do
+    # `bucket` is now bucket from the setup block
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+end
--- test/kv_test.exs
+++ test/kv_test.exs
@@ -3,6 +3,6 @@ defmodule KVTest do
   doctest KV
 
   test "greets the world" do
-    assert KV.hello() == :oop
+    assert KV.hello() == :world
   end
 end
